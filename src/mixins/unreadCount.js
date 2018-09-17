aab5ca04ed7cf9b95e1f73ee281d7cec1c48194e
yaojinhui <yaojinhui1993@gmail.com>
2018-09-17 17:08:25 +0800

    unread notifications
    1. setTabBarBadge
    2. removeTabBarBadge

---
+++ src/mixins/unreadCount.js
@@ -0,0 +1,34 @@
+import wepy from 'wepy'
+
+export default class unreadCount extends wepy.mixin {
+  data = {
+    interval: null,
+    unreadCount: 0
+  }
+
+  onShow() {
+    this.updateUnreadCount()
+    this.interval = setInterval(() => this.updateUnreadCount(), 30000)
+  }
+
+  onHide() {
+    clearInterval(this.interval)
+  }
+
+  updateUnreadCount() {
+    this.unreadCount = this.$parent.globalData.unreadCount
+    this.$apply()
+
+    if (this.unreadCount) {
+      wepy.setTabBarBadge({
+        index: 1,
+        text: this.unreadCount.toString()
+      })
+      return
+    }
+
+    wepy.removeTabBarBadge({
+      index: 1
+    })
+  }
+}
